010b1c064fe554ac3fcbb800bb5c2f47df1f6940
Mona-Lee Braga <codewith.monalee@gmail.com>
2023-01-19 11:54:12 -0300

    Aula 3

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('#form-habits')
+const nlwSetup = new NLWSetup(form)
+
+const data = {
+  run: ['01-01', '01-02', '01-06'],
+  water: ['01-04', '01-05'],
+  food: ['01-01', '01-03'],
+}
+
+nlwSetup.setData(data)
+nlwSetup.load()
