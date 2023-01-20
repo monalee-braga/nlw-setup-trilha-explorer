072885bf5b53dc932465a29dd2d841f3a6a3108d
Mona-Lee Braga <codewith.monalee@gmail.com>
2023-01-20 10:53:24 -0300

    Aula 4

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-  run: ['01-01', '01-02', '01-06'],
-  water: ['01-04', '01-05'],
-  food: ['01-01', '01-03'],
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add() {
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert('Dia já incluso!')
+    return
+  }
+
+  alert('Dia adicionado com sucesso ✅')
+  nlwSetup.addDay(today)
 }
 
-nlwSetup.setData(data)
-nlwSetup.load()
+function save() {
+  localStorage.clear()
+  const key = window.btoa('NLWSetup@habits')
+  const value = window.btoa(JSON.stringify(nlwSetup.data))
+  localStorage.setItem(key, value)
+}
+
+function get() {
+  const key = window.btoa('NLWSetup@habits')
+  const value = JSON.parse(window.atob(localStorage.getItem(key))) || {}
+  nlwSetup.setData(value)
+  nlwSetup.load()
+}
+
+get()
